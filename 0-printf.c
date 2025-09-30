233359abe80a37d2c2529fb22d06490dd8464dba
NadaGhannam25 <nada.ghannam19@gmail.com>
2025-09-30 05:32:45 +0300

    Update 0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -1,49 +1,153 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
 
 /**
  * _printf - produces output according to a format
  * @format: format string containing format specifiers
  * Return: number of characters printed (excluding null byte)
  */
+static int _putchar(char c)
+{
+    if (write(1, &c, 1) != 1)
+    {
+        return (-1);
+    }
+    return (1);
+}
+
+/**
+ * _putstr - write a string to stdout
+ * @s: the string to print, prints "(null)" if NULL
+ *
+ * Return: number of characters printed, -1 on error
+ */
+static int _putstr(char *s)
+{
+    unsigned int len = 0;
+
+    if (s == NULL)
+    {
+        s = "(null)";
+    }
+
+    while (s[len])
+    {
+        len++;
+    }
+
+    if (len == 0)
+    {
+        return (0);
+    }
+
+    if (write(1, s, len) != (ssize_t)len)
+    {
+        return (-1);
+    }
+
+    return ((int)len);
+}
+
+/**
+ * _printf - simplified printf supporting %c, %s and %%
+ * @format: format string containing characters and format specifiers
+ *
+ * Description: This function prints characters and strings to the
+ * standard output. It handles three format specifiers:
+ * %c for character, %s for string, and %% for percent sign.
+ *
+ * Return: number of characters printed, or -1 on error
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
+    va_list ap;
     int count = 0;
+    unsigned int i;
+    int res;
 
-    if (!format || (format[0] == '%' && format[1] == '\0'))
-        return (-1);
-
-    va_start(args, format);
-    while (*format)
+    if (format == NULL)
     {
-        if (*format == '%')
+        return (-1);
+    }
+
+    va_start(ap, format);
+
+    for (i = 0; format[i] != '\0'; i++)
+    {
+        if (format[i] != '%')
         {
-            format++;
-            if (*format == '\0')
+            res = _putchar(format[i]);
+            if (res < 0)
             {
-                va_end(args);
+                va_end(ap);
                 return (-1);
             }
-            if (*format == 'c')
-                count += _putchar(va_arg(args, int));
-            else if (*format == 's')
-                count += _putstr(va_arg(args, char *));
-            else if (*format == '%')
-                count += _putchar('%');
-            else if (*format == 'd' || *format == 'i')
-                count += _print_number(va_arg(args, long));
-            else
+            count += res;
+            continue;
+        }
+
+        i++;
+        if (format[i] == '\0')
+        {
+            va_end(ap);
+            return (-1);
+        }
+
+        if (format[i] == 'c')
+        {
+            char c = (char)va_arg(ap, int);
+
+            res = _putchar(c);
+            if (res < 0)
             {
-                count += _putchar('%');
-                count += _putchar(*format);
+                va_end(ap);
+                return (-1);
             }
+            count += res;
+        }
+        else if (format[i] == 's')
+        {
+            char *s = va_arg(ap, char *);
+
+            res = _putstr(s);
+            if (res < 0)
+            {
+                va_end(ap);
+                return (-1);
+            }
+            count += res;
+        }
+        else if (format[i] == '%')
+        {
+            res = _putchar('%');
+            if (res < 0)
+            {
+                va_end(ap);
+                return (-1);
+            }
+            count += res;
         }
         else
         {
-            count += _putchar(*format);
+            res = _putchar('%');
+            if (res < 0)
+            {
+                va_end(ap);
+                return (-1);
+            }
+            count += res;
+
+            res = _putchar(format[i]);
+            if (res < 0)
+            {
+                va_end(ap);
+                return (-1);
+            }
+            count += res;
         }
-        format++;
     }
-    va_end(args);
+
+    va_end(ap);
     return (count);
 }
