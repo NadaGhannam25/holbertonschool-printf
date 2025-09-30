b111c948417d3b31cfb77a7eb299a03ab23ba9c4
root <Nada.Ghannam19@gmail.com>
2025-09-30 04:31:24 +0000

    add

--- main.h
+++ main.h
@@ -1,29 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <limits.h>
-#include <stdio.h>
+
 #include <stdarg.h>
-#include <stdlib.h>
 #include <unistd.h>
 
-/**
- * struct format - converter for printf
- * @ph: type char pointer of the specifier
- * @function: function for the conversion specifier
- *
- */
-
-typedef struct format
-{
-	char *ph;
-	int (*function)();
-} convert;
-
-int print_i(va_list args);
-int print_d(va_list args);
-int print_c(va_list val);
-int print_s(va_list val);
 int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list val);
+int print_s(va_list val);
+int print_i(va_list args);
+int print_d(va_list args);
 
 #endif
+
