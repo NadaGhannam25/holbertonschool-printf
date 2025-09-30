663029b0a5236477ef06401facc5f0e8399533e1
NadaGhannam25 <nada.ghannam19@gmail.com>
2025-09-30 04:52:46 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdarg.h>
 #include <unistd.h>
+#include <limits.h>
+
+int _putchar(char c);
+
+int _putstr(char *s);
+
 int _printf(const char *format, ...);
-#endif /* MAIN_H */
+
+#endif
