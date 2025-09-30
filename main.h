408cbf9dc038e7d9fd5bd5c9eb25e283ea57cc23
root <Nada.Ghannam19@gmail.com>
2025-09-30 04:32:39 +0000

    add

--- main.h
+++ main.h
@@ -1,15 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_c(va_list val);
-int print_s(va_list val);
-int print_i(va_list args);
-int print_d(va_list args);
+/* Print functions */
+int print_HEX(unsigned int num);
+int print_HEX_extra(unsigned int num);
+int print_bin(unsigned int num);
+int print_exc_string(const char *s);
+int print_hex(unsigned int num);
+int print_hex_extra(unsigned int num);
+int print_oct(unsigned int num);
+int print_pointer(void *ptr);
+int print_rot13(const char *s);
+int print_srev(const char *s);
+int print_str(const char *s);
+int print_uns(unsigned int num);
 
-#endif
+#endif /* MAIN_H */
 
