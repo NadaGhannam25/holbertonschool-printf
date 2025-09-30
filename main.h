fba9959fd6a68649b73100b175ed31ea7e2f86ba
root <Nada.Ghannam19@gmail.com>
2025-09-30 14:44:27 +0000

    add

--- main.h
+++ main.h
@@ -1,17 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
 #include <stdarg.h>
 #include <unistd.h>
 
-/* Print functions */
+/**
+ * struct format - Struct for specifiers
+ * @ph: the format specifier
+ * @function: the function associated
+ */
+typedef struct format
+{
+    char *ph;
+    int (*function)(va_list);
+} convert;
+
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_c(va_list val);
 int print_s(va_list val);
 int print_i(va_list args);
 int print_d(va_list args);
+int print_37(void);
 
-#endif /* MAIN_H */
+#endif
 
