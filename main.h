f846ec511191bcfc77a47331f5ef261b418f8145
root <Nada.Ghannam19@gmail.com>
2025-10-02 21:31:13 +0000

    add

--- main.h
+++ main.h
@@ -3,21 +3,22 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
+#include <stddef.h>
 
-/* _printf function */
+#define BUFSIZE 1024
+
 int _printf(const char *format, ...);
 
-/* Helpers */
-int _putc(char c);
-int _puts(const char *s);
-int handle_flags(char *s, char flag);
-int handle_precision(char *s, int prec, int is_number);
-int print_number(long n, int base, int is_unsigned, int is_upper, int prec);
-int print_string(char *s, int prec);
-int print_pointer(void *p);
-int print_custom_S(char *s);
+/* buffer helpers */
+int flush_buffer(char *buf, int *idx);
+int buf_append(char *buf, int *idx, const char *s, int len);
+int buf_putc(char *buf, int *idx, char c);
 
-#endif
+/* conversion helpers */
+int print_number(char *buf, int *idx, long n);
+int print_unsigned_num_base(char *buf, int *idx, unsigned long n,
+                            int base, int uppercase);
+int print_pointer(char *buf, int *idx, void *p);
+
+#endif /* MAIN_H */
 
