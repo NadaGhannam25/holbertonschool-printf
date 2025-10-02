50c49d2df14ca3c83c4a15130ba84ae6c858e679
root <Nada.Ghannam19@gmail.com>
2025-10-02 00:33:58 +0000

    add

--- _printf.c
+++ _printf.c
@@ -1,17 +1,4 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-
-/**
- * _putc - write one character to stdout
- * @c: character
- *
- * Return: 1 on success
- */
-static int _putc(char c)
-{
-    return (write(1, &c, 1) == 1 ? 1 : 0);
-}
 
 /**
  * _puts - write a C string to stdout
@@ -21,133 +8,83 @@ static int _putc(char c)
  */
 static int _puts(const char *s)
 {
-    int n = 0;
+	int n = 0;
 
-    if (!s)
-        s = "(null)";
-    while (s[n])
-        n++;
-    if (n)
-        write(1, s, n);
-    return (n);
+	if (!s)
+		s = "(null)";
+	while (s[n])
+		n++;
+	if (n)
+		write(1, s, n);
+	return (n);
 }
 
 /**
- * _puts_int - write an integer to stdout
- * @n: integer to print
+ * _putc - write one character to stdout
+ * @c: character
  *
- * Return: number of characters printed
+ * Return: 1 on success
  */
-static int _puts_int(int n)
+static int _putc(char c)
 {
-    char buf[12];
-    int i = 0, j, count = 0;
-    unsigned int num;
-
-    if (n < 0)
-    {
-        count += _putc('-');
-        num = -n;
-    }
-    else
-        num = n;
-
-    if (num == 0)
-        return count + _putc('0');
-
-    while (num > 0)
-    {
-        buf[i++] = (num % 10) + '0';
-        num /= 10;
-    }
-
-    for (j = i - 1; j >= 0; j--)
-        count += _putc(buf[j]);
-
-    return count;
+	return (write(1, &c, 1) == 1 ? 1 : 0);
 }
 
 /**
- * _puts_unsigned - print unsigned int in decimal
- * @n: unsigned int
- *
- * Return: number of characters printed
- */
-static int _puts_unsigned(unsigned int n)
-{
-    char buf[11];
-    int i = 0, j, count = 0;
-
-    if (n == 0)
-        return _putc('0');
-
-    while (n > 0)
-    {
-        buf[i++] = (n % 10) + '0';
-        n /= 10;
-    }
-
-    for (j = i - 1; j >= 0; j--)
-        count += _putc(buf[j]);
-
-    return count;
-}
-
-/**
- * _puts_base - print unsigned int in given base
+ * _putnbr - print integer
  * @n: number
- * @base: base (8 for octal, 16 for hex)
- * @uppercase: 1 if uppercase hex, 0 if lowercase
  *
  * Return: number of characters printed
  */
-static int _puts_base(unsigned int n, int base, int uppercase)
+static int _putnbr(int n)
 {
-    char buf[32];
-    char *digits;
-    int i = 0, j, count = 0;
+	unsigned int num;
+	int count = 0;
 
-    digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
+	if (n < 0)
+	{
+		count += _putc('-');
+		num = -n;
+	}
+	else
+		num = n;
 
-    if (n == 0)
-        return _putc('0');
+	if (num / 10)
+		count += _putnbr(num / 10);
 
-    while (n > 0)
-    {
-        buf[i++] = digits[n % base];
-        n /= base;
-    }
-
-    for (j = i - 1; j >= 0; j--)
-        count += _putc(buf[j]);
-
-    return count;
+	count += _putc((num % 10) + '0');
+	return (count);
 }
 
 /**
- * _puts_binary - write an unsigned int in binary
- * @n: number to print
+ * _putnbr_base - print unsigned int in any base
+ * @n: number
+ * @base: base (e.g., 2, 8, 10, 16)
+ * @uppercase: flag for uppercase hex
  *
  * Return: number of characters printed
  */
-static int _puts_binary(unsigned int n)
+static int _putnbr_base(unsigned int n, int base, int uppercase)
 {
-    char buf[32];
-    int i = 0, j, count = 0;
+	char buffer[32];
+	char *digits;
+	int i = 0, count = 0;
 
-    if (n == 0)
-        return _putc('0');
+	digits = uppercase ? "0123456789ABCDEF" : "0123456789abcdef";
 
-    while (n > 0)
-    {
-        buf[i++] = (n % 2) + '0';
-        n /= 2;
-    }
+	if (n == 0)
+		return (_putc('0'));
 
-    for (j = i - 1; j >= 0; j--)
-        count += _putc(buf[j]);
+	while (n > 0)
+	{
+		buffer[i++] = digits[n % base];
+		n /= base;
+	}
 
-    return count;
+	while (i > 0)
+		count += _putc(buffer[--i]);
+
+	return (count);
 }
 
 /**
@@ -159,58 +96,59 @@ static int _puts_binary(unsigned int n)
  */
 static int print_conv(char sp, va_list *ap)
 {
-    if (sp == 'c')
-        return (_putc((char)va_arg(*ap, int)));
-    if (sp == 's')
-        return (_puts(va_arg(*ap, char *)));
-    if (sp == '%')
-        return (_putc('%'));
-    if (sp == 'd' || sp == 'i')
-        return (_puts_int(va_arg(*ap, int)));
-    if (sp == 'b')
-        return (_puts_binary(va_arg(*ap, unsigned int)));
-    if (sp == 'u')
-        return (_puts_unsigned(va_arg(*ap, unsigned int)));
-    if (sp == 'o')
-        return (_puts_base(va_arg(*ap, unsigned int), 8, 0));
-    if (sp == 'x')
-        return (_puts_base(va_arg(*ap, unsigned int), 16, 0));
-    if (sp == 'X')
-        return (_puts_base(va_arg(*ap, unsigned int), 16, 1));
+	if (sp == 'c')
+		return (_putc((char)va_arg(*ap, int)));
+	if (sp == 's')
+		return (_puts(va_arg(*ap, char *)));
+	if (sp == '%')
+		return (_putc('%'));
+	if (sp == 'd' || sp == 'i')
+		return (_putnbr(va_arg(*ap, int)));
+	if (sp == 'b')
+		return (_putnbr_base(va_arg(*ap, unsigned int), 2, 0));
+	if (sp == 'u')
+		return (_putnbr_base(va_arg(*ap, unsigned int), 10, 0));
+	if (sp == 'o')
+		return (_putnbr_base(va_arg(*ap, unsigned int), 8, 0));
+	if (sp == 'x')
+		return (_putnbr_base(va_arg(*ap, unsigned int), 16, 0));
+	if (sp == 'X')
+		return (_putnbr_base(va_arg(*ap, unsigned int), 16, 1));
 
-    return (_putc('%') + _putc(sp));
+	/* unknown specifier: print '%' then the char */
+	return (_putc('%') + _putc(sp));
 }
 
 /**
  * _printf - produces output according to a format
- * @format: format string (supports %c, %s, %%, %d, %i, %b, %u, %o, %x, %X)
+ * @format: format string
  *
  * Return: number of characters printed, or -1 on error
  */
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int count = 0, i = 0;
+	va_list ap;
+	int count = 0, i = 0;
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    va_start(ap, format);
-    while (format[i])
-    {
-        if (format[i] != '%')
-            count += _putc(format[i++]);
-        else
-        {
-            i++;
-            if (!format[i])
-            {
-                va_end(ap);
-                return (-1);
-            }
-            count += print_conv(format[i++], &ap);
-        }
-    }
-    va_end(ap);
-    return (count);
+	va_start(ap, format);
+	while (format[i])
+	{
+		if (format[i] != '%')
+			count += _putc(format[i++]);
+		else
+		{
+			i++;
+			if (!format[i])
+			{
+				va_end(ap);
+				return (-1);
+			}
+			count += print_conv(format[i++], &ap);
+		}
+	}
+	va_end(ap);
+	return (count);
 }
