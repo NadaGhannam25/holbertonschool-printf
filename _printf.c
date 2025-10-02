e57260b54f9ab40183ca6e17620b36fbb046f04a
root <Nada.Ghannam19@gmail.com>
2025-10-02 03:05:08 +0000

    Add

--- _printf.c
+++ _printf.c
@@ -1,124 +1,125 @@
 #include "main.h"
 #include <stdarg.h>
-#include <limits.h>
-
-/* forward declaration */
-static int print_conv(char sp, va_list *ap, int flag_plus, int flag_space, int flag_hash);
 
 /**
- * _printf - formatted output with +, space, #
- * @format: format string
- * Return: chars printed
+ * print_conv - handles a single conversion specifier with length modifiers
+ * @fmt: format string
+ * @i: pointer to current index in format string
+ * @ap: variadic list
+ *
+ * Return: number of characters printed
  */
-int _printf(const char *format, ...)
+static int print_conv(const char *fmt, int *i, va_list *ap)
 {
-	va_list ap;
-	int count = 0, i = 0;
-	int flag_plus, flag_space, flag_hash;
+    int count = 0;
+    char sp = fmt[*i];
+    char modifier = 0; /* 0 = none, 'h', 'l' */
 
-	if (!format)
-		return (-1);
+    /* check for length modifier */
+    if (sp == 'h' || sp == 'l')
+    {
+        modifier = sp;
+        (*i)++;
+        sp = fmt[*i];
+    }
 
-	va_start(ap, format);
-	while (format[i])
-	{
-		if (format[i] != '%')
-			count += _putc_buffered(format[i++]);
-		else
-		{
-			i++;
-			flag_plus = flag_space = flag_hash = 0;
+    /* handle specifiers */
+    if (sp == 'd' || sp == 'i')
+    {
+        if (modifier == 'l')
+            count = _puts_number(va_arg(*ap, long), 10, 0);
+        else if (modifier == 'h')
+            count = _puts_number((short)va_arg(*ap, int), 10, 0);
+        else
+            count = _puts_number(va_arg(*ap, int), 10, 0);
+    }
+    else if (sp == 'u')
+    {
+        if (modifier == 'l')
+            count = _puts_number(va_arg(*ap, unsigned long), 10, 0);
+        else if (modifier == 'h')
+            count = _puts_number((unsigned short)va_arg(*ap, unsigned int), 10, 0);
+        else
+            count = _puts_number(va_arg(*ap, unsigned int), 10, 0);
+    }
+    else if (sp == 'o')
+    {
+        if (modifier == 'l')
+            count = _puts_number(va_arg(*ap, unsigned long), 8, 0);
+        else if (modifier == 'h')
+            count = _puts_number((unsigned short)va_arg(*ap, unsigned int), 8, 0);
+        else
+            count = _puts_number(va_arg(*ap, unsigned int), 8, 0);
+    }
+    else if (sp == 'x')
+    {
+        if (modifier == 'l')
+            count = _puts_number(va_arg(*ap, unsigned long), 16, 0);
+        else if (modifier == 'h')
+            count = _puts_number((unsigned short)va_arg(*ap, unsigned int), 16, 0);
+        else
+            count = _puts_number(va_arg(*ap, unsigned int), 16, 0);
+    }
+    else if (sp == 'X')
+    {
+        if (modifier == 'l')
+            count = _puts_number(va_arg(*ap, unsigned long), 16, 1);
+        else if (modifier == 'h')
+            count = _puts_number((unsigned short)va_arg(*ap, unsigned int), 16, 1);
+        else
+            count = _puts_number(va_arg(*ap, unsigned int), 16, 1);
+    }
+    else if (sp == 'c')
+        count = _putc((char)va_arg(*ap, int));
+    else if (sp == 's')
+        count = _puts(va_arg(*ap, char *));
+    else if (sp == '%')
+        count = _putc('%');
+    else if (sp == 'b')
+        count = _puts_number(va_arg(*ap, unsigned int), 2, 0);
+    else if (sp == 'S')
+        count = _puts_non_printable(va_arg(*ap, char *));
+    else if (sp == 'p')
+        count = _puts_pointer(va_arg(*ap, void *));
+    else
+        count = _putc('%') + _putc(sp);
 
-			/* read flags */
-			while (format[i] == '+' || format[i] == ' ' || format[i] == '#')
-			{
-				if (format[i] == '+')
-					flag_plus = 1;
-				else if (format[i] == ' ')
-					flag_space = 1;
-				else if (format[i] == '#')
-					flag_hash = 1;
-				i++;
-			}
-
-			if (!format[i])
-			{
-				va_end(ap);
-				return (-1);
-			}
-
-			count += print_conv(format[i++], &ap, flag_plus, flag_space, flag_hash);
-		}
-	}
-	flush_buffer();
-	va_end(ap);
-	return (count);
+    return (count);
 }
 
 /**
- * print_conv - handle conversion specifiers with flags
+ * _printf - produces output according to a format
+ * @format: format string
+ *
+ * Return: number of characters printed, or -1 on error
  */
-static int print_conv(char sp, va_list *ap, int flag_plus, int flag_space, int flag_hash)
+int _printf(const char *format, ...)
 {
-	int count = 0;
-	long n;
-	unsigned int num;
-	char c;
+    va_list ap;
+    int count = 0, i = 0;
 
-	switch (sp)
-	{
-	case 'c':
-		c = (char)va_arg(*ap, int);
-		count += _putc_buffered(c);
-		break;
-	case 's':
-		count += _puts(va_arg(*ap, char *));
-		break;
-	case '%':
-		count += _putc_buffered('%');
-		break;
-	case 'd':
-	case 'i':
-		n = va_arg(*ap, long);
-		count += _puts_number(n, 10, 0, flag_plus, flag_space);
-		break;
-	case 'u':
-		num = va_arg(*ap, unsigned int);
-		count += _puts_number(num, 10, 0, 0, 0);
-		break;
-	case 'o':
-		num = va_arg(*ap, unsigned int);
-		if (flag_hash && num != 0)
-			count += _putc_buffered('0');
-		count += _puts_number(num, 8, 0, 0, 0);
-		break;
-	case 'x':
-		num = va_arg(*ap, unsigned int);
-		if (flag_hash && num != 0)
-		{
-			count += _putc_buffered('0');
-			count += _putc_buffered('x');
-		}
-		count += _puts_number(num, 16, 0, 0, 0);
-		break;
-	case 'X':
-		num = va_arg(*ap, unsigned int);
-		if (flag_hash && num != 0)
-		{
-			count += _putc_buffered('0');
-			count += _putc_buffered('X');
-		}
-		count += _puts_number(num, 16, 1, 0, 0);
-		break;
-	case 'b':
-		num = va_arg(*ap, unsigned int);
-		count += _puts_number(num, 2, 0, 0, 0);
-		break;
-	default:
-		count += _putc_buffered('%');
-		count += _putc_buffered(sp);
-		break;
-	}
-	return (count);
+    if (!format)
+        return (-1);
+
+    va_start(ap, format);
+    while (format[i])
+    {
+        if (format[i] != '%')
+            count += _putc(format[i++]);
+        else
+        {
+            i++;
+            if (!format[i])
+            {
+                va_end(ap);
+                return (-1);
+            }
+            count += print_conv(format, &i, &ap);
+            i++;
+        }
+    }
+    va_end(ap);
+
+    return (count);
 }
 
